4bb84aa172346bd394e46984f37d9d3dbe0c281e
Nick <tnick1502@mail.ru>
2023-10-31 17:39:25 +0300

    Update app.py

--- src/app/app.py
+++ src/app/app.py
@@ -46,9 +46,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],#["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
-    allow_headers=["*"],#["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-                   #"Authorization", "Accept", "X-Requested-With"],
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization", "Accept", "X-Requested-With"],
 )
 #["*"],#
 app.include_router(router)
